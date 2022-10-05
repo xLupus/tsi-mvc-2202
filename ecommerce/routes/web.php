24d9a9cf3d811c075ca0884b89f89e73eb161b4e
VINICIUS DO AMARAL SOUZA <vinicius.asouza34@senacsp.edu.br>
2022-10-04 22:12:45 -0300

    ACL - Access control list

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -9,23 +9,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/avisos', function(){
-  //Comandos Exception
-  $av = ['avisos' => [0 => [ 'data' => '06/09/202',
-                             'aviso' => 'Estudar PHP',
-                             'exibir' => true],
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-                       1 => ['data' => '09/09/202',
-                             'aviso' => ' Sexta-feira',
-                             'exibir' => false],
-
-                       2 => ['data' => '18/09/202',
-                             'aviso' => ' Meu Aniversario',
-                             'exibir' => true]]];
+require __DIR__.'/auth.php';
 
 
-  return view('avisos', $av);
-});
-Route::resource('/clientes', App\Http\Controllers\ClienteController::class);
-Route::resource('/vendedores', App\Http\Controllers\VendedoresController::class);
-Route::resource('/produtos', App\Http\Controllers\ProdutoController::class);
+Route::resource('/clientes', App\Http\Controllers\ClienteController::class)->middleware(['auth']);
+Route::resource('/vendedores', App\Http\Controllers\VendedoresController::class)->middleware(['auth']);
+Route::resource('/produtos', App\Http\Controllers\ProdutoController::class)->middleware(['auth']);
